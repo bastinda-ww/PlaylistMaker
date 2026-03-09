c6710f0750c9aa75553f7effa9fdd9b33fdde90d
bastinda-ww <andreidenisov96@gmail.com>
2026-03-09 11:16:35 +0400

    8 спринт тосты

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.maps3d.model.ImageView
+//import com.google.android.gms.maps3d.model.ImageView
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
@@ -20,25 +20,25 @@ class MainActivity : AppCompatActivity() {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
-//        val search_button = findViewById<MaterialButton>(R.id.search_button)
-//
-//        search_button.setOnClickListener {
-//            Toast.makeText(this@MainActivity, "Тост с маслом", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        val media_button = findViewById<MaterialButton>(R.id.media_button)
-//
-//        media_button.setOnClickListener {
-//            Toast.makeText(this@MainActivity, "Тост с джемом", Toast.LENGTH_SHORT).show()
-//        }
-//
-//        val settings_button = findViewById<MaterialButton>(R.id.settings_button)
-//
-//        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                Toast.makeText(this@MainActivity, "Тост за здоровье", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//        settings_button.setOnClickListener(imageClickListener)
+        val search_button = findViewById<MaterialButton>(R.id.search_button)
+
+        search_button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Тост с маслом", Toast.LENGTH_SHORT).show()
+        }
+
+        val media_button = findViewById<MaterialButton>(R.id.media_button)
+
+        media_button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Тост с джемом", Toast.LENGTH_SHORT).show()
+        }
+
+        val settings_button = findViewById<MaterialButton>(R.id.settings_button)
+
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Тост за здоровье", Toast.LENGTH_SHORT).show()
+            }
+        }
+        settings_button.setOnClickListener(imageClickListener)
     }
 }
