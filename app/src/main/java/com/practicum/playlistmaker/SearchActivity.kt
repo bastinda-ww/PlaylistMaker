dd8a0c32cbf4f68f2a9a4b70af2d982f4d39a770
bastinda-ww <andreidenisov96@gmail.com>
2026-04-13 23:18:11 +0400

    Домашка 9 Добавил взаимодействие со строкой поиска

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,20 +1,58 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        val searchField = findViewById<EditText>(R.id.search_field)
+        val clearSearchButton = findViewById<ImageView>(R.id.clear_button)
+        clearSearchButton.visibility = View.GONE
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        val textWatcher = object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearSearchButton.visibility = clearButtonVisibility(s)
+            }
+
+        }
+        searchField.addTextChangedListener(textWatcher)
+
+        clearSearchButton.setOnClickListener {
+            searchField.text.clear()
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchField.windowToken, 0)
+        }
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
