07715fc34ff2ecf4c68a3c68c9db230c545fd7d6
bastinda-ww <andreidenisov96@gmail.com>
2026-07-24 18:52:52 +0400

    Домашка 9 Правки по замечаниям и рекомендациям

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,8 +1,6 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
